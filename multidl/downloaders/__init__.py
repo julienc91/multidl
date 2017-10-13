dcda8a958f018efa6a5f3067d42f9b6ea7d4e7c9
julien <git@julienc.io>
2017-10-13 23:27:33 +0200

    local file downloader

--- multidl/downloaders/__init__.py
+++ multidl/downloaders/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from multidl.downloaders.local_file_downloader import LocalFileDownloader
 from multidl.downloaders.ftp_downloader import FtpDownloader
 from multidl.downloaders.http_downloader import HttpDownloader
 
 SCHEMES = {
+    'file': LocalFileDownloader,
     'ftp': FtpDownloader,
     'http': HttpDownloader,
     'https': HttpDownloader,
---
+++ multidl/downloaders/local_file_downloader.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import os
+import time
+from urllib.parse import urlparse
+
+from multidl.downloaders.abstract_downloader import AbstractDownloader
+from multidl.constants import DownloadState
+
+
+class LocalFileDownloader(AbstractDownloader):
+
+    CHUNK_SIZE = 1024 * 16
+
+    def __init__(self, url, output, **options):
+        super().__init__(url, output, **options)
+        self._download_length = 0
+        self._downloaded_length = 0
+
+    def start(self):
+        super().start()
+
+        parsed_url = urlparse(self.url)
+        path = parsed_url.path
+
+        self._download_length = os.path.getsize(path)
+
+        with open(self.output, 'wb') as fw, open(path, 'rb') as fr:
+            while True:
+                data = self.__get_chunk(fr)
+                if not data:
+                    break
+                self._downloaded_length += len(data)
+                fw.write(data)
+
+        if self.state == DownloadState.canceling:
+            self.state = DownloadState.canceled
+        elif self.state != DownloadState.error:
+            self.state = DownloadState.finished
+
+    def __get_chunk(self, f):
+        while self.state == DownloadState.paused:
+            time.sleep(0.1)
+        return f.read(self.CHUNK_SIZE)
+
+    def get_progress(self):
+        super().get_progress()
+        return self._downloaded_length, self._download_length
+
+    def cancel(self):
+        super().cancel()
+        try:
+            os.remove(self.output)
+        except OSError:
+            pass
