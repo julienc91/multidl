62f51b366c522b9ae4efc15411a7038b9a53ef5e
julien <git@julienc.io>
2017-10-20 22:59:01 +0200

    still adding tests

--- multidl/cli.py
+++ multidl/cli.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import argparse
 
 from multidl.download_manager import DownloadManager
@@ -7,18 +8,35 @@ from multidl.download_manager import DownloadManager
 
 def parse_args():
 
+    def positive_integer(value):
+        try:
+            value = int(value)
+            if value <= 0:
+                raise ValueError
+        except (TypeError, ValueError):
+            raise argparse.ArgumentTypeError("{} is not a positive integer".format(value))
+        return value
+
+    def readable_directory(value):
+        try:
+            if (not os.path.isdir(value) or
+                    not os.access(value, os.R_OK)):
+                raise ValueError
+        except (ValueError, OSError):
+            raise argparse.ArgumentTypeError("{} is not a valid directory".format(value))
+        return value
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', required=True,
                         type=argparse.FileType('r'),
                         help='A file listing the targetted urls, '
                              'one item per line.')
     parser.add_argument('-o', '--output-directory', required=True,
+                        type=readable_directory,
                         help='Output directory.')
-    parser.add_argument('-n', default=10, type=int,
+    parser.add_argument('-n', default=10, type=positive_integer,
                         help='Number of parallel downloads')
     args = parser.parse_args()
-    if args.n <= 0:
-        raise argparse.ArgumentError('n', 'n must be strictly positive')
     return args
 
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import pytest
+
 from multidl.cli import parse_args
 
 
@@ -15,3 +17,27 @@ def test_parse_args(monkeypatch, config_file, tempdir):
     assert args.n == 4
     assert args.config.name == config_file
     assert args.output_directory == tempdir
+
+
+def test_parse_args_missing_parameters(monkeypatch, config_file, tempdir):
+    args = [['multidl', '-n', '4', '-o', tempdir],
+            ['multidl', '-n', '4', '-c', config_file]]
+
+    for arg in args:
+        monkeypatch.setattr('sys.argv', arg)
+        with pytest.raises(SystemExit):
+            parse_args()
+
+
+def test_parse_args_invalid_parameters(monkeypatch, config_file, tempdir):
+    args = [['multidl', '-n', '0', '-c', config_file, '-o', tempdir],
+            ['multidl', '-n', '-1', '-c', config_file, '-o', tempdir],
+            ['multidl', '-n', 'a', '-c', config_file, '-o', tempdir],
+            ['multidl', '-n', '4', '-c', 'foo', '-o', tempdir],
+            ['multidl', '-n', '4', '-c', config_file, '-o', '/root'],
+            ['multidl', '-n', '4', '-c', config_file, '-o', 'foo']]
+
+    for arg in args:
+        monkeypatch.setattr('sys.argv', arg)
+        with pytest.raises(SystemExit):
+            parse_args()
