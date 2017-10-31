35cee5ac31963655ef06dcd5266f28eee1b0f9f0
julien <git@julienc.io>
2017-10-31 21:02:50 +0100

    making setup.py install work

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup
 setup(
     name='multidl',
     version='0.1',
-    packages=['multidl'],
+    packages=[
+        'multidl',
+        'multidl.constants',
+        'multidl.downloaders'
+    ],
     install_requires=[
         'requests',
         'tqdm',
@@ -18,5 +22,10 @@ setup(
     license='MIT',
     author='Julien Chaumont',
     author_email='multidl@julienc.io',
-    description='Download stuff in parallel'
+    description='Download stuff in parallel',
+    entry_points={
+        'console_scripts': [
+            'multidl = multidl.cli:main',
+        ],
+    },
 )
